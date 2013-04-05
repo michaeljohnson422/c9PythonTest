f86d7e16d6cfbdcdcfc3df3dc05705265fae84bb
mrj05 <michael.johnson422@gmail.com>
2013-04-05 00:01:50 -0300

    Create test.py
    This is one of the first tests.
    However, this is written in Python 2.7.3 and NOT Python3

---
+++ test.py
@@ -0,0 +1,10 @@
+print "Type 3 to continue, anything else to quit." 
+someInput = raw_input()
+x = 3
+while x == 3:
+        while someInput == '3': 
+                print "Thank you for the 3. Very kind of you." 
+                print "Type 3 to continue."
+                someInput = raw_input() 
+        print "That's not 3, ENTER A 3!!!"
+        someInput = raw_input()
